80cb4dbd0718ef06cfb9e97d72e31cb15036e09e
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-27 00:36:19 +0700

    feat: show questions when clicking the start button

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const questions = [
+    {
+        question: "26 + 5 = ?",
+        answer: [
+            { text: "31", correct: true },
+            { text: "30", correct: false },
+            { text: "265", correct: false },
+            { text: "32", correct: false },
+        ],
+    },
+];
+
+let shuffledQuestions, currentQuestionIndex;
+
+// DOM elements
 const startBtn = document.getElementById("start-btn");
 const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
 
 startBtn.addEventListener("click", startGame);
 
@@ -10,8 +27,23 @@ function startGame() {
     startBtn.classList.add("hide");
     // reveal the question & buttons container
     questionContainerElement.classList.remove("hide");
+
+    // shuffle the questions
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    // set the current question index to 0
+    currentQuestionIndex = 0;
+
+    // reveal the next question
+    setNextQuestion();
 }
 
-function setNextQuestion() {}
+function showQuestion(question) {
+    // set the question text
+    questionElement.innerText = question.question;
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
 
 function selectAnswer() {}
